0cfd6bbc00ec9dde4ebcd2171f8741b966747aae
Kim Hojeong <hojeong0713@gmail.com>
2021-05-18 00:28:15 +0900

    Study Enum type

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -6,16 +6,29 @@
 //   age: 27
 // };
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: 'hoki',
+//   age: 27,
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'auther']
+// }
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role { ADMIN = 5, READ_ONLY = 8, AUTHOR = 10 }
+
+const person = {
   name: 'hoki',
   age: 27,
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'auther']
+  role: Role.ADMIN
 }
 
 // person.role.push('admin');
@@ -28,4 +41,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toLocaleUpperCase())
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('is Admin')
 }
