942fdac6f7bcb3d405a01e7499605232b1a022ac
Kim Hojeong <hojeong0713@gmail.com>
2021-05-17 23:28:44 +0900

    Study array type

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -8,7 +8,15 @@
 
 const person = {
   name: 'hoki',
-  age: 27
+  age: 27,
+  hobbies: ['Sports', 'Cooking']
 }
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toLocaleUpperCase())
+}
