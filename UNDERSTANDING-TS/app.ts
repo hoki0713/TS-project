160f4c0d2b51646fa830961da57151314b9a47b9
Kim Hojeong <hojeong0713@gmail.com>
2021-05-18 00:18:57 +0900

    Study Tuple type

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -6,11 +6,20 @@
 //   age: 27
 // };
 
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'hoki',
   age: 27,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'auther']
 }
+
+// person.role.push('admin');
+// person.role[1] = 10;
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
