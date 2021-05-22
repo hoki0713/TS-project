ac571ecfb35396c78d3511808be661ba4f7cdfcd
Kim Hojeong <hojeong0713@gmail.com>
2021-05-23 02:47:11 +0900

    Study never type

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -9,3 +9,12 @@ if (typeof userInput === 'string') {
   userName = userInput;
 }
 
+function generateError(message: string, code: number): never {
+  throw {
+    message: message,
+    errorCode: code
+  };
+  // while (true) {}
+}
+
+generateError('An error occurred!', 500);
