b55ff7bd477a333d5c67ccf9a536a10e78e704a2
Kim Hojeong <hojeong0713@gmail.com>
2021-05-24 02:41:22 +0900

    Study tsconfig.json

--- UNDERSTANDING-TS/app.ts
+++ UNDERSTANDING-TS/app.ts
@@ -1,20 +1,5 @@
-// let userInput: any;
-let userInput: unknown;
-let userName: string;
+const button = document.querySelector('button')!;
 
-userInput = 5;
-userInput = 'Max';
-
-if (typeof userInput === 'string') {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw {
-    message: message,
-    errorCode: code
-  };
-  // while (true) {}
-}
-
-generateError('An error occurred!', 500);
+button.addEventListener('click', () => {
+  console.log('Clicked!');
+});
--- UNDERSTANDING-TS/functions.ts
+++ UNDERSTANDING-TS/functions.ts
@@ -1,8 +1,8 @@
-function add(n1: number, n2: number): number {
+function addFunction(n1: number, n2: number): number {
   return n1 + n2;
 }
 
-function printResult(num: number): void {
+function printResultFunction(num: number): void {
   console.log('Result : ' + num);
 }
 
@@ -11,13 +11,13 @@ function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
   cb(result);
 }
 
-printResult(add(5, 12));
+printResultFunction(addFunction(5, 12));
 
 // let combineValues: Function;
 let combineValues: (a: number, b: number) => number;
 
-combineValues = add;
-// combineValues = printResult;
+combineValues = addFunction;
+// combineValues = printResultFunction;
 // combineValues = 5;
 console.log(combineValues(8, 8));
 
