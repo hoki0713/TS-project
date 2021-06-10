2607fe0909b3219b6fd3bf69e84cdbd7b19ca381
Kim Hojeong <hojeong0713@gmail.com>
2021-06-10 15:04:20 +0200

    Study class

--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -1,11 +1,37 @@
 class Department {
-  name: string;
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
   
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}) : ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('d1', 'Accounting');
 
-console.log(accounting)
+accounting.addEmployee('Hoki');
+accounting.addEmployee('Doki');
+
+// accounting.employee[3] = 'Name';
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: 'Sales', describe: accounting.describe};
+
+// accountingCopy.describe();
