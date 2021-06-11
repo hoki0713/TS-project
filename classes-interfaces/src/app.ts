76b0ef20d1e6726ed92c3a1cd9ada032e5bb7368
Kim Hojeong <hojeong0713@gmail.com>
2021-06-11 16:04:34 +0200

    Study singletons and private constructor

--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -43,6 +43,7 @@ class ITDepartment extends Department {
 
 class AccountingDepartment extends Department {
   private lastReport: string;
+  private static instance: AccountingDepartment;
 
   get mostRecentReport() {
     if (this.lastReport) {
@@ -58,9 +59,18 @@ class AccountingDepartment extends Department {
     this.addReport(value);
   }
 
-  constructor(id: string, private reports: string[]) {
+  private constructor(id: string, private reports: string[]) {
     super(id, 'Account');
     this.lastReport = reports[0];
+  }
+
+  static getInstance() {
+    if (AccountingDepartment.instance) {
+      return this.instance;
+    }
+    
+    this.instance = new AccountingDepartment('d2', []);
+    return this.instance;
   }
 
   describe() {
@@ -102,7 +112,11 @@ it.printEmployeeInformation();
 
 // accountingCopy.describe();
 
-const accounting = new AccountingDepartment('d2', []);
+// const accounting = new AccountingDepartment('d2', []);
+const accounting = AccountingDepartment.getInstance();
+const accounting2 = AccountingDepartment.getInstance();
+
+console.log(accounting, accounting2);
 
 accounting.mostRecentReport = 'Year End Report';
 accounting.addReport('Something report...');
